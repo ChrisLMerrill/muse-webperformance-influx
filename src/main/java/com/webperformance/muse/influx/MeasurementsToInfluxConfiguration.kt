a937e1ab80d8ec40273c1456e73a28d165e47177
ChrisLMerrill <chris@ide4selenium.com>
2019-06-17 12:00:22 -0400

    provide defaults for the configuration parameters

--- src/main/java/com/webperformance/muse/influx/MeasurementsToInfluxConfiguration.kt
+++ src/main/java/com/webperformance/muse/influx/MeasurementsToInfluxConfiguration.kt
@@ -1,7 +1,7 @@
 package com.webperformance.muse.influx
 
 import com.webperformance.muse.measurements.*
-import org.musetest.builtins.value.*
+import org.musetest.builtins.value.collection.*
 import org.musetest.core.*
 import org.musetest.core.plugins.*
 import org.musetest.core.resource.generic.*
@@ -14,8 +14,8 @@ import org.musetest.core.values.descriptor.*
 	MuseSubsourceDescriptor(displayName = "Apply automatically?", description = "If this source resolves to true, this plugin configuration will be automatically applied to tests", type = SubsourceDescriptor.Type.Named, name = GenericConfigurablePlugin.AUTO_APPLY_PARAM),
 	MuseSubsourceDescriptor(displayName = "Apply only if", description = "Apply only if this source this source resolves to true", type = SubsourceDescriptor.Type.Named, name = GenericConfigurablePlugin.APPLY_CONDITION_PARAM),
 	MuseSubsourceDescriptor(displayName = "Hostname", description = "Hostname of Influx server", type = SubsourceDescriptor.Type.Named, name = MeasurementsToInfluxConfiguration.HOSTNAME_PARAM),
-	MuseSubsourceDescriptor(displayName = "Port", description = "Port on Influx server (defaults to 8086)", type = SubsourceDescriptor.Type.Named, name = MeasurementsToInfluxConfiguration.PORT_PARAM, optional = true),
-	MuseSubsourceDescriptor(displayName = "Database", description = "Name of Influx database to send measurements to", type = SubsourceDescriptor.Type.Named, name = MeasurementsToInfluxConfiguration.DATABASE_PARAM, optional = true),
+	MuseSubsourceDescriptor(displayName = "Port", description = "Port on Influx server (defaults to 8086)", type = SubsourceDescriptor.Type.Named, name = MeasurementsToInfluxConfiguration.PORT_PARAM, optional = true, defaultValue = MeasurementsToInfluxConfiguration.PORT_PARAM_DEFAULT),
+	MuseSubsourceDescriptor(displayName = "Database", description = "Name of Influx database to send measurements to", type = SubsourceDescriptor.Type.Named, name = MeasurementsToInfluxConfiguration.DATABASE_PARAM, optional = true, defaultValue = "telegraf"),
 	MuseSubsourceDescriptor(displayName = "Ignore Metadata", description = "Measurement metadata attributes to ignore", type = SubsourceDescriptor.Type.List, name = MeasurementsToInfluxConfiguration.IGNORE_META_PARAM, optional = true)
 )
 class MeasurementsToInfluxConfiguration : GenericResourceConfiguration(), PluginConfiguration
@@ -34,7 +34,7 @@ class MeasurementsToInfluxConfiguration : GenericResourceConfiguration(), Plugin
 	{
 		val hostname_source_config = parameters[HOSTNAME_PARAM]
 		if (hostname_source_config == null)
-			return null;
+			return null
 		
 		val hostname_source = hostname_source_config.createSource()
 		return BaseValueSource.getValue(hostname_source, context, false, String::class.java)
@@ -42,7 +42,7 @@ class MeasurementsToInfluxConfiguration : GenericResourceConfiguration(), Plugin
 	
 	fun getPort(context : MuseExecutionContext): Int
 	{
-		var port = 8086
+		var port = PORT_PARAM_DEFAULT.toInt()
 
 		val port_source_config = parameters[PORT_PARAM]
 		if (port_source_config != null)
@@ -52,7 +52,7 @@ class MeasurementsToInfluxConfiguration : GenericResourceConfiguration(), Plugin
 			if (value != null)
 				port = value.toInt()
 		}
-		return port;
+		return port
 	}
 	
 	fun getDatabase(context : MuseExecutionContext): String
@@ -67,7 +67,7 @@ class MeasurementsToInfluxConfiguration : GenericResourceConfiguration(), Plugin
 			if (value != null)
 				database = value
 		}
-		return database;
+		return database
 	}
 	
 	fun getIgnoreMetadata(context: MuseExecutionContext): List<String>
@@ -113,5 +113,6 @@ class MeasurementsToInfluxConfiguration : GenericResourceConfiguration(), Plugin
 		const val PORT_PARAM = "port"
 		const val DATABASE_PARAM = "database"
 		const val IGNORE_META_PARAM = "ignore_meta"
+		const val PORT_PARAM_DEFAULT = "8086"
 	}
 }
